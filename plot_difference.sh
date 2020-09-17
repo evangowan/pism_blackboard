9f3a52b92c5e2ec4fa77d9170610d47278b275ac
evangowan <evangowan@gmail.com>
2020-09-17 17:06:03 +0200

    prints filenames on the plot

--- plot_difference.sh
+++ plot_difference.sh
@@ -10,6 +10,7 @@ y_max=4000000
 
 half_tick=2000000
 eighth_tick=500000
+sixteeth_tick=250000
 
 R_options="-R${x_min}/${x_max}/${y_min}/${y_max}"
 
@@ -34,6 +35,11 @@ grdmath diff.nc ${max_diff} LT diff2.nc ${max_diff} IFELSE = diff3.nc
 plot=thickness_difference.ps
 
 grdimage diff3.nc -Xa${x_position} -Ya${y_position}  ${R_options} ${J_options} -Bx+l"x (m)" -By+l"y (m)"  -Ba${half_tick}f${eighth_tick} -BSWen -K -P -Cshades_diff.cpt -V -nb > ${plot}
+
+pstext << END  -Ya${y_position} -Xa${x_position}  ${R_options} ${J_options} -O -K -P -F+f10p,Helvetica,black+cTL -D0.1/-0.1 -Gwhite >> ${plot}
+${file1} - ${file2}
+END
+
 
 x_position="6c"
 y_position="5.5c"
@@ -66,6 +72,10 @@ x_position="3c"
 y_position="8c"
 
 grdimage diff3.nc -Xa${x_position} -Ya${y_position}  ${R_options} ${J_options} -Bx+l"x (m)" -By+l"y (m)"  -Ba${half_tick}f${eighth_tick} -BSWen -K -P -Cshades_diff.cpt -V -nb > ${plot}
+
+pstext << END  -Ya${y_position} -Xa${x_position}  ${R_options} ${J_options} -O -K -P -F+f10p,Helvetica,black+cTL -D0.1/-0.1 -Gwhite >> ${plot}
+${file1} - ${file2}
+END
 
 x_position="6c"
 y_position="5.5c"
