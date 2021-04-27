12d1fae4be4ce275378cd67334b9e58be187f6b7
evangowan <evangowan@gmail.com>
2021-04-27 16:26:55 +0200

    reduced size of label font

--- velocity_ts_plot.sh
+++ velocity_ts_plot.sh
@@ -46,7 +46,7 @@ plot="velocity_compare.ps"
 
 gmt psxy ts_1.txt  ${J_options} ${R_options} -P -K -Wthick,blue > ${plot}
 
-gmt psxy ts_2.txt  -BWSen -Bxa${xint}f${xsubint}+l"Time (years)" -Bya${yint}f${ysubint}+l"Velocity (m/yr)"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+gmt psxy ts_2.txt  -BWSen -Bxa${xint}f${xsubint}+l"Time (years)" -Bya${yint}f${ysubint}+l"Velocity (m/yr)"   ${J_options} ${R_options} -P -O -K -Wthick,red --FONT_LABEL=10p >> ${plot}
 
 gmt psxy << END_CAT ${J_options} ${R_options}  -P -K -O -Wthick,blue >> ${plot}
 25041 27
@@ -76,7 +76,7 @@ R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
 
 gmt psxy ts_1.txt -Y${height}  ${J_options} ${R_options} -O -P -K -Wthick,blue >> ${plot}
 
-gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Sliding mechanism"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Sliding mechanism"   ${J_options} ${R_options} -P -O -K -Wthick,red --FONT_LABEL=10p >> ${plot}
 
 python3 ${path}/extract_ts_hydrology.py ${y1} ${y2}
 
@@ -91,7 +91,7 @@ R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
 
 gmt psxy ts_1.txt -Y${height}  ${J_options} ${R_options} -P -K -O -Wthick,blue >> ${plot}
 
-gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Hydrology type"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Hydrology type"   ${J_options} ${R_options} -P -O -K -Wthick,red  --FONT_LABEL=10p >> ${plot}
 
 
 
@@ -112,6 +112,6 @@ gmt psxy ts_1_m.txt -Y${height}  ${J_options} ${R_options} -P -O -K -Wthick,blue
 
 awk -v minval=${ymin} '{if($2 < minval) {print $1, minval} else {print $1, $2}}' ts_2.txt > ts_2_m.txt
 
-gmt psxy ts_2_m.txt  -BWSen  -Bya1f3p+l"Water volume flux (m@+3@+/s)"   ${J_options} ${R_options} -P -O  -Wthick,red >> ${plot}
+gmt psxy ts_2_m.txt  -BWSen  -Bya1f3p+l"Water volume flux (m@+3@+/s)"   ${J_options} ${R_options} -P -O  -Wthick,red --FONT_LABEL=10p >> ${plot}
 
 
