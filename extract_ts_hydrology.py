7056d9ea89f26ae789495c0f6b4315ddbc4a1d16
evangowan <evangowan@gmail.com>
2021-04-27 16:17:02 +0200

    added to the velocity plot other factors related to hydrology

---
+++ extract_ts_hydrology.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python3
+
+import numpy as np
+import netCDF4
+import csv
+
+import sys
+
+
+yval = sys.argv[1:]
+
+filename="ex_pism.nc"
+
+
+
+
+# desired station time series location
+
+x_loc = 2000000
+
+nc = netCDF4.Dataset(filename)
+nc.variables
+
+x = nc.variables['x'][:]
+y = nc.variables['y'][:]
+times = nc.variables['time']
+times2= nc.variables['time'][:]
+jd = netCDF4.num2date(times[:],times.units)
+
+def near(array,value):
+    idx=(np.abs(array-value)).argmin()
+    return idx
+
+
+counter=0
+
+for y_loc in yval:
+
+	counter += 1
+
+
+	# find nearest point to desired location
+	ix = near(x, x_loc)
+	iy = near(y, float(y_loc))
+
+	# get all time records of variable [vname] at indices [iy,ix]
+	vname = 'hydrology_type'
+	var = nc.variables[vname]
+	h = var[:,iy,ix]
+
+	multiplied_time = times2 / (365.0 * 24.0 * 3600.0)
+
+
+
+	combined = np.column_stack((np.array(multiplied_time), np.array(h)))
+
+	filename=f"ts_{counter}.txt"
+	print(filename)
+
+	np.savetxt(filename,combined, delimiter=" ")
+
--- velocity_ts_plot.sh
+++ velocity_ts_plot.sh
@@ -2,6 +2,8 @@
 
 path="/work/ollie/egowan/PISM/pism_blackboard"
 
+height=4c
+
 percent_cover=$1
 
 xmin=25040
@@ -13,7 +15,7 @@ ymax=30
 yint=10
 ysubint=5
 
-J_options="-JX12c/4c"
+J_options="-JX12c/${height}"
 R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
 
 x=2000000
@@ -44,7 +46,7 @@ plot="velocity_compare.ps"
 
 gmt psxy ts_1.txt  ${J_options} ${R_options} -P -K -Wthick,blue > ${plot}
 
-gmt psxy ts_2.txt  -BWSen -Bxa${xint}f${xsubint}+l"Time (years)" -Bya${yint}f${ysubint}+l"Ice surface velocity (m/yr)"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+gmt psxy ts_2.txt  -BWSen -Bxa${xint}f${xsubint}+l"Time (years)" -Bya${yint}f${ysubint}+l"Velocity (m/yr)"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
 
 gmt psxy << END_CAT ${J_options} ${R_options}  -P -K -O -Wthick,blue >> ${plot}
 25041 27
@@ -56,7 +58,60 @@ gmt psxy << END_CAT ${J_options} ${R_options}  -P -K -O -Wthick,red >> ${plot}
 25042 22
 END_CAT
 
-gmt pstext << END_CAT  ${J_options} ${R_options} -P  -O -F+f10p,Helvetica,black+jlm >> ${plot}
+gmt pstext << END_CAT  ${J_options} ${R_options} -P -K -O -F+f10p,Helvetica,black+jlm >> ${plot}
 25042.5 27 ${percent_cover}% cover
 25042.5 22 100% cover
 END_CAT
+
+python3 ${path}/extract_ts_sliding.py ${y1} ${y2}
+
+
+
+ymin=0
+ymax=4
+yint=1
+ysubint=5
+
+R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
+
+gmt psxy ts_1.txt -Y${height}  ${J_options} ${R_options} -P -K -Wthick,blue > ${plot}
+
+gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Sliding mechanism"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+
+python3 ${path}/extract_ts_hydrology.py ${y1} ${y2}
+
+
+
+ymin=0
+ymax=4
+yint=1
+ysubint=5
+
+R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
+
+gmt psxy ts_1.txt -Y${height}  ${J_options} ${R_options} -P -K -Wthick,blue > ${plot}
+
+gmt psxy ts_2.txt  -BWSen  -Bya${yint}+l"Hydrology type"   ${J_options} ${R_options} -P -O -K -Wthick,red >> ${plot}
+
+
+
+python3 ${path}/extract_ts_waterflux.py ${y1} ${y2}
+
+ymin=1
+ymax=10000
+yint=1
+ysubint=5
+
+R_options="-R${xmin}/${xmax}/${ymin}/${ymax}"
+
+J_options="-JX12c/${height}l"
+
+awk -v minval=${ymin} '{if($2 < minval) {print $1, minval} else {print $1, $2}}' ts_1.txt > ts_1_m.txt
+
+gmt psxy ts_1_m.txt -Y${height}  ${J_options} ${R_options} -P -K -Wthick,blue > ${plot}
+
+awk -v minval=${ymin} '{if($2 < minval) {print $1, minval} else {print $1, $2}}' ts_2.txt > ts_2_m.txt
+
+gmt psxy ts_2_m.txt  -BWSen  -Byal+l"Water volume flux (m@+3@+/s)"   ${J_options} ${R_options} -P -O  -Wthick,red >> ${plot}
+
+
