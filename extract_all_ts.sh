58edbbc2c6252862e2c40b45ccbd4d067936e9e1
Evan Gowan <evangowan@gmail.com>
2022-06-20 16:39:07 +0900

    added new sub experiments

--- extract_all_ts.sh
+++ extract_all_ts.sh
@@ -87,3 +87,81 @@ fi
 
 done
 done
+
+
+# extract extra experiments
+
+for experiment in  new_basal_50_cover_lowangle2_phi20_wet3 
+do
+
+for subexperiment in higher high_20000 high_30000 high_35000 high_40000
+directory=${path}/${experiment}/${subexperiment}
+
+if [ -d "$directory" ]; then
+
+cd ${directory}
+
+mkdir ${path}/time_series/${experiment}_${subexperiment}
+
+vartype=thk
+python3 ${path}/extract_ts.py ${y1} ${y2}
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+vartype=volume_water_flux
+python3 ${path}/extract_ts_waterflux.py ${y1} ${y2}
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+
+vartype=velsurf_mag
+python3 ${path}/extract_ts_vel.py ${y1} ${y2}
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+
+vartype=hydrology_type
+python3 ${path}/extract_ts_hydrology.py ${y1} ${y2}
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+
+vartype=usurf
+python3 ${path}/extract_ts_usurf.py ${y1} ${y2}
+
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+
+
+
+vartype=sliding_mechanism
+python3 ${path}/extract_ts_sliding.py ${y1} ${y2}
+
+
+for counter in 1 2
+do
+cp ts_${vartype}_${counter}.txt ${path}/time_series/${experiment}_${subexperiment}/ts_${vartype}_${counter}.txt
+done
+
+cd ${path}
+
+fi
+
+done
+done
