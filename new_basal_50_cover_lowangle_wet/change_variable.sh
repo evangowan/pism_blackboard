8f52b2bf3ac75bb7c8b1c72a082581ac7e654787
Evan Gowan <evangowan@gmail.com>
2022-06-21 17:58:59 +0900

    forgot to add this one

---
+++ new_basal_50_cover_lowangle_wet/change_variable.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+
+variable=tillcover
+
+gmt grdmath pism_start.nc?${variable} 0 MUL = zero_grid.nc
+
+replace_value=0.5
+
+low_range=0.4
+high_range=0.6
+
+gmt grdmath zero_grid.nc  X  ADD XMAX XMIN SUB ${low_range} MUL XMIN ADD XMAX XMIN SUB ${high_range} MUL XMIN ADD INRANGE = x_values.nc
+
+low_range=0
+high_range=0.45
+gmt grdmath zero_grid.nc  Y  ADD YMAX YMIN SUB ${low_range} MUL YMIN ADD YMAX YMIN SUB ${high_range} MUL YMIN ADD INRANGE = y_values.nc
+
+gmt grdmath x_values.nc y_values.nc MUL = mask.nc
+
+gmt grdmath  mask.nc ${replace_value} pism_start.nc?${variable} IFELSE = ${variable}_replace.nc
+ncrename -O -v z,${variable} ${variable}_replace.nc
+
+ncap -O -s "${variable}=double(${variable});"  ${variable}_replace.nc ${variable}_replace2.nc
+
+
+cdo -O merge  pism_start.nc ${variable}_replace2.nc testing.nc
+
+ncap2 -Oh -s "${variable}=${variable}_2;" testing.nc testing2.nc
+
+ncks -O -x -v ${variable}_2 testing2.nc testing3.nc
+
+mv -f testing3.nc pism_start.nc 
+
+rm  testing.nc testing2.nc ${variable}_replace2.nc ${variable}_replace.nc y_values.nc x_values.nc mask.nc zero_grid.nc tillcover_temp.nc
